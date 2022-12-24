a467f577d05ec833d29f7d51aca68f2bfef1160a
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2022-12-24 18:05:06 -0300

    Project models.

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Payment
+
+
 # Register your models here.
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('title', 'value',)
+
+
+admin.site.register(Payment, PaymentAdmin)
